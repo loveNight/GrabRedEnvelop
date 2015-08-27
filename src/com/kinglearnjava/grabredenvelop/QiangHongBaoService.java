e0d6ff038a445f49c46ebf0423e5f83262a8c435
kinglearnjava <286242151@qq.com>
2015-08-27 16:31:16 +0800

    开始写自定义Log工具LogUtils，未完成，晚上继续

--- src/com/kinglearnjava/grabredenvelop/QiangHongBaoService.java
+++ src/com/kinglearnjava/grabredenvelop/QiangHongBaoService.java
@@ -1,6 +1,7 @@
 package com.kinglearnjava.grabredenvelop;
 
 import android.accessibilityservice.AccessibilityService;
+import android.os.Handler;
 import android.view.accessibility.AccessibilityEvent;
 
 /**
@@ -8,9 +9,25 @@ import android.view.accessibility.AccessibilityEvent;
  * 对照 https://github.com/lendylongli/qianghongbao 所写
  */
 public class QiangHongBaoService extends AccessibilityService {
+    
+    private static final String TAG = "QiangHongBao";
+    
+    // 微信包名
+    private static final String WECHAT_PACKAGENAME = "com.tencent.mm";
+    
+    // 红包消息关键字
+    private static final String HONGBAO_TEXT_KEY = "[微信红包]";
+    
+    private Handler handler = new Handler();
 
+    /**
+     * 必须重载的方法
+     * 接收系统发来的AccessbilityEvent，已经按照配置文件过滤
+     */
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
+        final int eventType = event.getEventType();
+        
     }
 
     @Override
