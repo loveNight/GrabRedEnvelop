6132212759bd619f64f45ed035c39899ca66a4d8
kinglearnjava <286242151@qq.com>
2015-09-06 21:00:19 +0800

    解决了重复领取同一个红包的问题，未解决当前界面收到新红包却不会领取的问题

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-有重复领取红包的BUG，打算加上未领取标记
-还没找到合适的办法来标记不同的红包
+
+
+##存在的问题
+- 无法判断一个红包是否已经被领取过，更深层次的原因是切换界面时，无法区别不同的红包。所以目前只能做到领取最新红包。
+1.使用AccessibilityNodeInfo获取的红包消息中不包含时间，无法使用时间来区别。
+2.这个AccessibilityNodeInfo对象又会被重复用在不同的红包上，无法使用对象的ClassName+hashCode来区别。
+    <1>同一个聊天界面中同时显示的不同红包，用不同的AccessibilityNodeInfo对象表示，但是每次切换到该界面，这些对象代表的红包都会变化
+    <1>同一个聊天界面中不能同时显示的两个不同红包，用相同的AccessibilityNodeInfo对象表示
+    
+- 当前聊天界面收到红包时，不会被触发。
+尝试用AccessibilityEvent.TYPE_VIEW_CLICKED事件来检查新红包，但是在红包详情界面一样会触发此事件，涉及红包重复判断的逻辑，暂时取消此事件的监听
+
+##事件测试
+####在聊天界面收到当前好友的一条消息时：
+触发事件---->TYPE_WINDOW_CONTENT_CHANGED
+事件的包名---->com.tencent.mm
+事件的类名---->android.widget.ListView
+事件的文本---->
+触发事件---->TYPE_VIEW_SCROLLED
+事件的包名---->com.tencent.mm
+事件的类名---->android.widget.ListView
+事件的文本---->
+触发事件---->TYPE_WINDOW_CONTENT_CHANGED
+事件的包名---->com.tencent.mm
+事件的类名---->android.widget.ListView
+事件的文本---->
+触发事件---->TYPE_WINDOW_CONTENT_CHANGED
+事件的包名---->com.tencent.mm
+事件的类名---->android.widget.ListView
+事件的文本---->
+触发事件---->TYPE_WINDOW_CONTENT_CHANGED
+事件的包名---->com.tencent.mm
+事件的类名---->android.widget.ListView
+事件的文本---->
+
+####在聊天界面收到其他好友的一条消息时：
+触发事件---->TYPE_WINDOW_CONTENT_CHANGED
+事件的包名---->com.tencent.mm
+事件的类名---->android.widget.ListView
+事件的文本---->
+触发事件---->TYPE_NOTIFICATION_STATE_CHANGED
+事件的包名---->com.tencent.mm
+事件的类名---->android.app.Notification
+事件的文本---->好友昵称: 测试消息
+
+####换了Activity，才会触发AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED。而从微信的消息列表界面到聊天界面，并不触发此事件，说明没有切换Activity
--- src/com/kinglearnjava/grabredenvelop/QiangHongBaoService.java
+++ src/com/kinglearnjava/grabredenvelop/QiangHongBaoService.java
@@ -1,8 +1,6 @@
 package com.kinglearnjava.grabredenvelop;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import android.accessibilityservice.AccessibilityService;
 import android.annotation.TargetApi;
@@ -18,20 +16,35 @@ import android.widget.Toast;
 
 /**
  * 抢红包外挂服务
- * 对照 https://github.com/lendylongli/qianghongbao 所写
+ * 对照 https://github.com/lendylongli/qianghongbao 所写，有改进
  */
 public class QiangHongBaoService extends AccessibilityService {
     
+    // 调试用TAG
     private static final String TAG = "QiangHongBao";
+    private static final String TAG_EVENT_TEST = "事件测试";
+    private static final String TAG_NODE = "AccessibilityNodeInfo测试";
     
     // 微信包名
     private static final String WECHAT_PACKAGENAME = "com.tencent.mm";
     
-    // 红包消息关键字
+    // 通知栏红包消息关键字
     private static final String HONGBAO_TEXT_KEY = "[微信红包]";
     
-    // 已领取红包
-    private Set<String> robbedBonus = new HashSet<String>();
+    // 拆红包界面的按钮关键字
+    private static final String GET_HONGBAO_KEY = "拆红包";
+    
+    // 聊天界面别人发的红包的关键字
+    private static final String OPEN_OTHERS_HONGBAO_KEY = "领取红包";
+    
+    // 聊天界面自己发的红包的关键字
+    private static final String OPEN_SELF_HONGBAO_KEY = "查看红包";
+    
+    // 上一个界面是红包详情
+    // 退出红包详情界面，进入聊天界面，触发TYPE_WINDOW_STATE_CHANGED事件，有两种情况：
+    // 一、界面中有红包，如果此标记为true，则不领取，同时把此标记改为false
+    // 二、界面中无红包，则把此标记改为false
+    private boolean lastWindowHongBaoDetail = false;
     
     private Handler handler = new Handler();
 
@@ -43,6 +56,66 @@ public class QiangHongBaoService extends AccessibilityService {
     public void onAccessibilityEvent(AccessibilityEvent event) {
         final int eventType = event.getEventType();
         LogUtil.d(TAG, "onAccessibilityEvent-----现在触发的事件---->" + event);
+        
+        // 测试用，看看微信中各种动作会触发什么事件
+        String eventText = "";
+        String eventPackageName = String.valueOf(event.getPackageName());
+        List<CharSequence> eventTexts = event.getText(); 
+        String eventClassName = String.valueOf(event.getClassName());
+        switch (eventType) {  
+        case AccessibilityEvent.TYPE_VIEW_CLICKED:  
+            eventText = "TYPE_VIEW_CLICKED";  
+            break;  
+        case AccessibilityEvent.TYPE_VIEW_FOCUSED:  
+            eventText = "TYPE_VIEW_FOCUSED";  
+            break;  
+        case AccessibilityEvent.TYPE_VIEW_LONG_CLICKED:  
+            eventText = "TYPE_VIEW_LONG_CLICKED";  
+            break;  
+        case AccessibilityEvent.TYPE_VIEW_SELECTED:  
+            eventText = "TYPE_VIEW_SELECTED";  
+            break;  
+        case AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED:  
+            eventText = "TYPE_VIEW_TEXT_CHANGED";  
+            break;  
+        case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:  
+            eventText = "TYPE_WINDOW_STATE_CHANGED";  
+            break;  
+        case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:  
+            eventText = "TYPE_NOTIFICATION_STATE_CHANGED";  
+            break;  
+        case AccessibilityEvent.TYPE_TOUCH_EXPLORATION_GESTURE_END:  
+            eventText = "TYPE_TOUCH_EXPLORATION_GESTURE_END";  
+            break;  
+        case AccessibilityEvent.TYPE_ANNOUNCEMENT:  
+            eventText = "TYPE_ANNOUNCEMENT";  
+            break;  
+        case AccessibilityEvent.TYPE_TOUCH_EXPLORATION_GESTURE_START:  
+            eventText = "TYPE_TOUCH_EXPLORATION_GESTURE_START";  
+            break;  
+        case AccessibilityEvent.TYPE_VIEW_HOVER_ENTER:  
+            eventText = "TYPE_VIEW_HOVER_ENTER";  
+            break;  
+        case AccessibilityEvent.TYPE_VIEW_HOVER_EXIT:  
+            eventText = "TYPE_VIEW_HOVER_EXIT";  
+            break;  
+        case AccessibilityEvent.TYPE_VIEW_SCROLLED:  
+            eventText = "TYPE_VIEW_SCROLLED";  
+            break;  
+        case AccessibilityEvent.TYPE_VIEW_TEXT_SELECTION_CHANGED:  
+            eventText = "TYPE_VIEW_TEXT_SELECTION_CHANGED";  
+            break;  
+        case AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED:  
+            eventText = "TYPE_WINDOW_CONTENT_CHANGED";  
+            break;  
+        }  
+        LogUtil.i(TAG_EVENT_TEST, "触发事件---->" + eventText);
+        LogUtil.i(TAG_EVENT_TEST, "事件的包名---->" + eventPackageName);
+        LogUtil.i(TAG_EVENT_TEST, "事件的类名---->" + eventClassName);
+        LogUtil.i(TAG_EVENT_TEST, "事件的文本---->");
+        for (CharSequence s : eventTexts) {
+            LogUtil.i(TAG_EVENT_TEST,  String.valueOf(s));
+        }
         
         // 通知栏事件
         if (eventType == AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED) {
@@ -60,9 +133,9 @@ public class QiangHongBaoService extends AccessibilityService {
                 }
             }
         } else if (eventType == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
-            // 窗口改变（不一定是微信，下面会判断界面），则调动打开红包方法 
+            // 所监听的APP的Activity改变时，触发AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED
             openHongBao(event);
-        }
+        } 
     }
 
     
@@ -89,6 +162,9 @@ public class QiangHongBaoService extends AccessibilityService {
         Toast.makeText(this, "连接抢红包服务", Toast.LENGTH_SHORT).show();
     }
     
+    /**
+     * 发送通知栏信息，模拟微信红包信息。
+     */
     private void sendNotifyEvent() {
         AccessibilityManager manager = (AccessibilityManager)getSystemService(ACCESSIBILITY_SERVICE);
         if (!manager.isEnabled()) {
@@ -124,22 +200,26 @@ public class QiangHongBaoService extends AccessibilityService {
     /**
      * 打开微信后，判断是什么界面，并做相应的动作
      */
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     private void openHongBao(AccessibilityEvent event) {
-        // 领取红包界面
         if ("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyReceiveUI".equals(event.getClassName())) {
-            // 领取红包
+            // 拆红包界面
             getPacket();
         } else if ("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyDetailUI".equals(event.getClassName())) {
             // 拆完红包后看详细的纪录界面
-            // 什么都不做
-        } else if ("com.tencent.mm.ui.LauncherUI".equals(event.getClassName())) {
+            LogUtil.e(TAG, "红包详情界面，改变lastWindowHongBaoDetail，此时值为" + lastWindowHongBaoDetail);
+            lastWindowHongBaoDetail = true;
+        } else if ("com.tencent.mm.ui.LauncherUI".equals(event.getClassName()) ) {
             // 聊天界面
             openPacket();
-        }
+//            // 测试用，输出当前界面AccessibilityNodeInfo对象的所有信息
+//            AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
+//            recycle(nodeInfo);
+        } 
     }
     
     /**
-     * 拆红包
+     * 在拆红包界面中打开红包
      */
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     private void getPacket() {
@@ -148,7 +228,7 @@ public class QiangHongBaoService extends AccessibilityService {
             LogUtil.w(TAG, "getPacket()------rootWindow为空");
             return;
         }
-        List<AccessibilityNodeInfo> list = nodeInfo.findAccessibilityNodeInfosByText("拆红包");
+        List<AccessibilityNodeInfo> list = nodeInfo.findAccessibilityNodeInfosByText(GET_HONGBAO_KEY);
         for (AccessibilityNodeInfo n :list) {
             LogUtil.v(TAG + "拆红包", "getPacket()-------->微信红包--------->" + n);
             n.performAction(AccessibilityNodeInfo.ACTION_CLICK);
@@ -156,43 +236,43 @@ public class QiangHongBaoService extends AccessibilityService {
     }
     
     /**
-     * 在聊天界面中点红包
+     * 在聊天界面中打开红包
+     * 由AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED触发 
      */
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     private void openPacket() {
         AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
+        LogUtil.d(TAG, "RootWindow的ClassName:" + nodeInfo.getClassName());
         if (nodeInfo == null) {
             LogUtil.w(TAG + "聊天界面", "openPacket()-------->rootWindow为空");
             return;
         }
-        // 找到领取红包的点击事件
-        List<AccessibilityNodeInfo> list = nodeInfo.findAccessibilityNodeInfosByText("领取红包");
-        if (list.isEmpty()) {
-            list = nodeInfo.findAccessibilityNodeInfosByText(HONGBAO_TEXT_KEY);
-            for(AccessibilityNodeInfo n : list) {
-                LogUtil.i(TAG + "聊天界面", "openPacket()-->微信红包:使用HONGBAO_TEXT_KEY------>" + n);
-                n.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-                break;
-            }
+        // 找到领取红包的点击事件，别人发的红包
+        List<AccessibilityNodeInfo> list = nodeInfo.findAccessibilityNodeInfosByText(OPEN_OTHERS_HONGBAO_KEY);
+        // 可能是自己发的红包
+        if (list.isEmpty()) {            
+            list = nodeInfo.findAccessibilityNodeInfosByText(OPEN_SELF_HONGBAO_KEY);
+        } 
+        // 仍没有红包或刚领完红包返回，则不领取，并改变标记
+        if (list.isEmpty() || lastWindowHongBaoDetail) {
+            LogUtil.e(TAG, "聊天界面，改变lastWindowHongBaoDetail，此时值为" + lastWindowHongBaoDetail);
+            lastWindowHongBaoDetail = false;
+            return;
         } else {
-            // 最新的红包领起
+            // 当前界面可能有多个红包，只领最新的。
             for (int i = list.size() - 1 ; i >= 0; i--) {
                 // 通过调试可知[领取红包]是text，本身不可被点击，用getParent()获取可被点击的对象
                 AccessibilityNodeInfo parent = list.get(i).getParent();
-                LogUtil.i(TAG + "聊天界面", "openPacket()---->遍历红包:输出parent------->" + parent);
-                LogUtil.i(TAG + "聊天界面", "openPacket()---->遍历红包:输出list------->" + list.get(i));
-//                LogUtil.e(TAG, "输出Set--->" + robbedBonus);
-//                // 谷歌重写了toString()方法，不能用它获取ClassName@hashCode串
-//                if ( (parent != null) && (!robbedBonus.contains(getOriginToString(parent))) ) {
-//                    robbedBonus.add(getOriginToString(parent));
-//                    parent.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-//                    LogUtil.e(TAG, "点击红包：" + getOriginToString(parent));
-//                    LogUtil.e(TAG, "输出Set--->" + robbedBonus);
-//                    break; // 只领最新的一个红包
-//                }
+                // 加个判断，以免空指针
+                if ( parent != null) {
+                    LogUtil.e(TAG, "点击红包：" + getOriginToString(parent));
+                    parent.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                    break; // 只领最新的一个红包
+                }
             }
         }
     }
+    
     
     /**
      * Java默认的toString()方法，用于识别是否同一个对象
@@ -200,6 +280,34 @@ public class QiangHongBaoService extends AccessibilityService {
      */
     private String getOriginToString(Object o) {
         return o.getClass().getName() + '@' + Integer.toHexString(o.hashCode());
+    }
+    
+    /**
+     * 遍历AccessibilityNodeInfo并输出
+     * @param nodeInfo
+     */
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+    private void recycle(AccessibilityNodeInfo nodeInfo) {
+        LogUtil.i(TAG_NODE, "------------------------------------------");
+        LogUtil.i(TAG_NODE, "nodeInfo.getChildCount()----->" + nodeInfo.getChildCount());
+        LogUtil.i(TAG_NODE, "nodeInfo.getWindowId()----->" + nodeInfo.getWindowId());
+        LogUtil.i(TAG_NODE, "nodeInfo.getClassName()----->" + nodeInfo.getClassName());
+        LogUtil.i(TAG_NODE, "nodeInfo.getContentDescription()----->" + nodeInfo.getContentDescription());
+        LogUtil.i(TAG_NODE, "nodeInfo.getPackageName()----->" + nodeInfo.getPackageName());
+        LogUtil.i(TAG_NODE, "nodeInfo.getText()----->" + nodeInfo.getText());
+//        LogUtil.i(TAG_NODE, "nodeInfo.getViewIdResourceName()----->" + nodeInfo.getViewIdResourceName());
+//        LogUtil.i(TAG_NODE, "nodeInfo.getActions()----->" + nodeInfo.getActions());
+//        LogUtil.i(TAG_NODE, "nodeInfo.getMovementGranularities()----->" + nodeInfo.getMovementGranularities());
+//        LogUtil.i(TAG_NODE, "nodeInfo.getgetTextSelectionEnd()----->" + nodeInfo.getTextSelectionEnd());
+//        LogUtil.i(TAG_NODE, "nodeInfo.getTextSelectionStart()----->" + nodeInfo.getTextSelectionStart());
+//        LogUtil.i(TAG_NODE, "nodeInfo.getViewIdResourceName()----->" + nodeInfo.getViewIdResourceName());
+//        LogUtil.i(TAG_NODE, "nodeInfo.getLabeledBy()----->" + nodeInfo.getLabeledBy());
+//        LogUtil.i(TAG_NODE, "nodeInfo.getLabelFor()----->" + nodeInfo.getLabelFor());
+        if (nodeInfo.getChildCount() > 0) {
+            for (int i = 0; i < nodeInfo.getChildCount(); i++) {
+                recycle(nodeInfo.getChild(i));
+            }
+        }
     }
 
 }
